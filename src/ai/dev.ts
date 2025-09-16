bd935ec8cbdfefdfeb7661ac9507b858b5a37c92
yonkekes <yer81al@gmail.com>
2025-09-16 16:58:36 -0400

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/get-spending-insights.ts';
-import '@/ai/flows/categorize-transaction.ts';
--- src/app/transactions/page.tsx
+++
@@ -1,5 +0,0 @@
-import TransactionsPage from "@/components/transactions/transactions-page";
-
-export default function Transactions() {
-    return <TransactionsPage />;
-}
